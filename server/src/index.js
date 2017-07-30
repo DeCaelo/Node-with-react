6681a428591c9cc25b554c6d8af4670a99aad015
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-07-30 15:30:55 +0200

    Mongoose model Classes User & Saving Model Instances

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+// this require is always before the services/passport
+// MongooseError: Schema hasn't been registered for model "users"
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
