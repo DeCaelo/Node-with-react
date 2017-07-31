5de33d7053688500f63bbc999c84281a8dc96069
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-07-31 08:12:39 +0200

    cookie session

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('../config/keys');
 // this require is always before the services/passport
 // MongooseError: Schema hasn't been registered for model "users"
@@ -9,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
