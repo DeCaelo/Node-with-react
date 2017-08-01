b773bf911472ab68782dc545e90cb10714ebddf3
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-08-01 17:17:05 +0200

    switch materialize => bootstrap

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
+const button = {
+  margin: "10px",
+};
+
 class Header extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
-            <ul className="right">
-              <li><a>Login With Goole</a></li>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand">Emaily</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <button type="button" className="btn btn-default navbar-btn" style={button}>Login With Google</button>
             </ul>
-      </div>
-  </nav>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,4 @@
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
