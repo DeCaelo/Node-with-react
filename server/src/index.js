487e549cb45c2dab2a1780824aedf62909668e80
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-08-07 14:19:14 +0200

    billingRoutes => Post request hnadlers & console.log(req.body) bodyparser req.body

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('../config/keys');
 // this require is always before the services/passport
 // MongooseError: Schema hasn't been registered for model "users"
@@ -11,6 +12,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/bilingRoutes')(app);
 
 // Dynamic Port Binding
 const PORT = process.env.PORT || 5000;
