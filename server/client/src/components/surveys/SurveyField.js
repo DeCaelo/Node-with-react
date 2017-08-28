f5e4be98bc015f480a59db655ddaf94488c7cd09
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-08-28 13:24:28 +0200

    custom field

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single lable & text input
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
