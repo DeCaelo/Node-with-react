18cdf47f6ac50b1f188eb11a32c2edc7ab88bc8b
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-03-08 10:50:08 +0100

    DashBoard refacto

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  width: 70px;
-  height: 70px;
-  padding: 10px 16px;
-  font-size: 24px;
-  line-height: 1.33;
-  border-radius: 35px;
-  position: absolute;
-  bottom: 30px;
-  right: 30px;
-`;
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
-      <Link to="/surveys/new">
-        <Button type="button" className="btn btn-danger btn-circle btn-lg">
-          <i className="glyphicon glyphicon-plus" />
-        </Button>
-      </Link>
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
     </div>
   );
 };
